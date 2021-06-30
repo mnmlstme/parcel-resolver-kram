706bf4fdb162b0b09d74a8cbc9e80856194fe113
Ken Kubiak <ken@mnmlst.me>
2021-06-29 17:36:30 -0700

    make kram work with elm

--- index.js
+++ index.js
@@ -1,39 +1,54 @@
 const { Resolver } = require('@parcel/plugin')
-const fs = require('fs').promises
+const fs = require('fs')
 const path = require('path')
-const querystring = require('querystring')
 const Kr = require('kram')
+
+const unstreamable = ['elm']
 
 module.exports = new Resolver ({
   async resolve({ filePath, dependency, logger }) {
-    const { moduleSpecifier, resolveFrom  } = dependency
+    const { resolveFrom  } = dependency
 
-    // can only resolve relative to a Kram file and requires query string
+    // can only resolve relative to a Kram file
     if ( resolveFrom.match(/\.kr$/) ) {
       logger.info({
-        message: `Kram: resolving import of ${moduleSpecifier} from ${resolveFrom}`,
+        message: `Kram: resolving import of ${filePath} from ${resolveFrom}`,
         filePath
       })
 
-      const file = await fs.readFile(resolveFrom)
-      const content = file.toString()
-      const { front, doc } = Kr.parse( content )
-      const { platform } = front
+      const resolveDir = path.dirname(resolveFrom)
 
-      const absolutePath = path.resolve(resolveFrom, moduleSpecifier)
-      const tail = absolutePath.replace(`${resolveFrom}/${platform}/`, '')
-      const regex = /^([A-Za-z]\w*)\.(\w+)$/
-      const matches = tail.match(regex)
+      const absolutePath = path.resolve(resolveDir, filePath)
+      const regex = /[#](\w+)\.(\w+)$/
+      const matches = absolutePath.match(regex)
 
       if ( matches ) {
           const [_, pkg, lang] = matches
           logger.info({
-              message: `Kram: collating ${lang} code for ${platform}`,
+              message: `Kram: extracting ${pkg}.${lang} from ${resolveFrom}`,
               filePath
           })
+
+          const kramFile = fs.readFileSync(resolveFrom)
+          const content = kramFile.toString()
+          const { front, doc } = Kr.parse( content )
           const code = doc.filter( t => t.type === "code" && t.lang === lang)
           const konfig = Kr.config(front, pkg)
           const generated = konfig.collate(code, lang)
+
+          if ( unstreamable.includes(lang) ) {
+              const hashkey = Kr.hashcode({front, doc}, lang).substr(-8)
+              const tmpDir = path.join(resolveDir, '.kram', hashkey)
+              const tmpFile = path.join(tmpDir, `${pkg}.${lang}`)
+              fs.mkdirSync(tmpDir, {recursive: true})
+              fs.writeFileSync(tmpFile, generated)
+
+              return {
+                  filePath: tmpFile,
+                  code: generated,
+                  lang
+              }
+          }
 
           return {
               filePath: absolutePath,
